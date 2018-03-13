679e8d616ab07f5c64527d325b96541a23a7b876
Christian Paun <christianpaun@gmail.com>
2018-03-13 19:28:37 +0000

    added axios support and tested connexion and response

--- src/components/Property/Property.js
+++ src/components/Property/Property.js
@@ -1,22 +1,12 @@
 import React from 'react';
+import classes from './Property.module.css';
+
 
 const property = (props)=>{
+
     return (
-        <div>
-            <ul>
-                <li>Type: FLAT</li>
-                <li>Rooms: 3</li>
-                <li>Description: CLEAR FLAT</li>
-                <li>Agent: 12addq-2312sa-dderwd</li>
-                <li>Address:</li>
-                <ul>
-                    <li>Street: 121 Green Close</li>
-                    <li>City: London</li>
-                    <li>Post Code: N10 5TL</li>
-                </ul>
-                <li>Size: 60</li>
-                <li>Price: £225.000</li>    
-            </ul>
+        <div className={classes.Property}>
+            {props.property}
         </div>    
     );
 };
--- src/containers/Estate/Estate.js
+++ src/containers/Estate/Estate.js
@@ -2,18 +2,66 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import classes from './Estate.module.css';
 import Property from '../../components/Property/Property';
+import axios from 'axios';
 
 class Estate extends Component {
     
     state = {
 
-    }
+    property: null,
+    error:false   
+    // property: {
+    //    type:"FLAT",
+    //    rooms:3,
+    //    description:"a clean unfurnished flat",
+    //    agent:"rightmove",
+    //    address: {
+    //        street: "121 Green Close",
+    //        city: "London",
+    //        postcode:"N10 5TL"
+    //    },
+    //    size: 60,
+    //    price: 225000
+    // }
+}
+
+componentDidMount () {
+    axios.get('http://mydomain.com:3000/properties/5aa80cac3d8f918cb4b2b15d')
+    .then(response=>{
+       console.log('response:', response.data)
+        this.setState({property: response.data})
+    })
+    .catch(error=>{
+        console.log('error:',error)
+        this.setState({error:true})
+    });
+}
 
     render (){
+        let property = this.state.error ? <p>details can't be loaded!</p> : <p>Loading...</p>
+        
+        if (this.state.property){
+            property=(
+                <ul>
+                <li><strong>Type:</strong> {this.state.property.type}</li>
+                <li><strong>Rooms:</strong> {this.state.property.rooms}</li>
+                <li><strong>Description:</strong> {this.state.property.description}</li>
+                <li><strong>Agent:</strong> {this.state.property.agent}</li>
+                <li><strong>Address:</strong></li>
+                <ul>
+                    <li><strong>Street:</strong> {this.state.property.address.street}</li>
+                    <li><strong>City:</strong> {this.state.property.address.city}</li>
+                    <li><strong>Post Code:</strong> {this.state.property.address.postcode}</li>
+                </ul>
+                <li><strong>Size:</strong> {this.state.property.size}sqm</li>
+                <li><strong>Price:</strong> £{this.state.property.price}</li>    
+            </ul>   
+            )}
+        
         return (
             <Aux>
             <p className={classes.Welcome}>Welcome to the Estate app</p>
-            <Property />
+            <Property property={property}/>
             </Aux>    
 
         );
